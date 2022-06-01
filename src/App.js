5516db5d57ef174ec4412f3bc8de627b17aca088
Umesh S M <umeshsmg007@gmail.com>
2022-06-01 18:15:06 +0530

    Basic flow of stopwatch

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
-import React, { useState, useEffect } from "react";
+import * as React from "react";
+
+const INTERVAL = 10;
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = React.useState(0);
+  const interval = React.useRef();
 
-  useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 100);
-  });
+  const handleStart = () => {
+    interval.current = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + INTERVAL);
+    }, INTERVAL);
+  };
 
- 
+  const handleStop = () => {
+    clearInterval(interval.current);
+  };
+
+  const handleReset = () => {
+    clearInterval(interval.current);
+    setCounter(0);
+  };
+
+  console.log(counter);
+
+  // counter --> milli seconds
+  // minutes => (counter / 60000) / 60
+  // seconds => (counter / 60000)
+
+  // slice values before showing them (value.slice(-2))
+  // calculation ==> Math.ceil / Math.floor
 
   return (
     <>
       <div className="container">
         <div className="timer">
           <h1 className="time">
-          <div className="timetext">
-          <span className="digit">00</span>
-            <span className="digit">Hr</span>
-            <span className="digit">00</span>
-            <span className="digit">Min</span>
-            <span className="digit">00</span>
-            <span className="digit">Sec</span>
-            <div className="digit">{count}</div>
-          </div>
+            <div className="timetext">
+              <span className="digit">00</span>
+              <span className="digit">m. </span>
+              <span className="digit">00</span>
+              <span className="digit">s. </span>
+              <div className="digit">00</div>
+              <span className="digit">ms</span>
+            </div>
           </h1>
-          
+
           <div className="buttoncontained">
-            <button className="btn">Start</button>
-            <button className="btn" value="stop">
+            <button className="btn" onClick={handleStart}>
+              Start
+            </button>
+            <button className="btn" value="stop" onClick={handleStop}>
               Stop
             </button>
-            <button className="btn">Reset</button>
+            <button className="btn" onClick={handleReset}>
+              Reset
+            </button>
           </div>
         </div>
       </div>
